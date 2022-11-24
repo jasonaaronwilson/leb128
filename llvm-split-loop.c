84c0d683506a643fbff2bf28a298a3c0f2b30e78
Jason A. Wilson <jasonaaronwilson@gmail.com>
2022-11-24 15:10:48 -0500

    remove some parens

--- llvm-split-loop.c
+++ llvm-split-loop.c
@@ -16,7 +16,7 @@ unsigned split_loop_encodeSLEB128(int64_t Value, uint8_t *p) {
       // NOTE: this assumes that this signed shift is an arithmetic right shift.
       Value >>= 7;
       Count++;
-      More = !(((Value == 0 ) && ((Byte & 0x40) == 0)));
+      More = !((Value == 0) && ((Byte & 0x40) == 0));
       if (More)
         Byte |= 0x80; // Mark this byte to show that more bytes will follow.
       *p++ = Byte;
@@ -28,7 +28,7 @@ unsigned split_loop_encodeSLEB128(int64_t Value, uint8_t *p) {
       // NOTE: this assumes that this signed shift is an arithmetic right shift.
       Value >>= 7;
       Count++;
-      More = !(((Value == -1) && ((Byte & 0x40) != 0)));
+      More = !((Value == -1) && ((Byte & 0x40) != 0));
       if (More)
         Byte |= 0x80; // Mark this byte to show that more bytes will follow.
       *p++ = Byte;
