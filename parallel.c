9473a285dca61aad802864671bdcb4e54ee2ad9e
Jason A. Wilson <jasonaaronwilson@gmail.com>
2022-11-25 06:19:22 -0500

    rearrange some of the if statements

--- parallel.c
+++ parallel.c
@@ -1,9 +1,16 @@
 #include "decoder.h"
 
-// Don't use this code since it is slow. The entire reason to use LEB
-// is because smaller numbers are much more common than large ones and
-// this optimized for large numbers. The benchmark shows that it is
-// probably more than twice as slow as the simpler code.
+// Don't use this code since it is not faster than the simpler smaller
+// implementations and hasn't been fully tested either.
+
+// The entire reason to use LEB is because smaller numbers are much
+// more common than large ones and this optimized for large
+// numbers. The original benchmark showed that it was almost twice as
+// slow as the simpler code though reorder the branch statements to
+// test for smaller valus first made it only a little slower than
+// similar routines (actually I'm comparing signed and unsigned cases
+// assuming they have similar speed when we know the unsigned case is
+// probably faster).
 
 /// Utility function to encode a SLEB128 value to a buffer. Returns
 /// the length in bytes of the encoded value.
@@ -20,14 +27,29 @@ unsigned parallel_encodeULEB128(int64_t Value, uint8_t *p) {
   uint64_t G = (Value << 6) & (0x7full << 48);
   uint64_t H = (Value << 7) & (0x7full << 56);
 
-  unsigned n_bytes = 1;
+  unsigned n_bytes;
 
   // This mostly looks like the intended result except the
   // continuation bits in the last bit of each byte.
   uint64_t result = A | B | C | D | E | F | G | H;
 
-  // TODO(jawilson): do the small tests first.
-  if (Value >= ((1ull << 49) - 1)) {
+  // TODO(jawilson): reorder the last few ones or turn into a binary
+  // search.
+  if (Value < ((1ull << 7) -1)) {
+    n_bytes = 1;
+  } else if (Value < ((1ull << 14) -1)) {
+    n_bytes = 2;
+    result |= (1ull << 7);
+  } else if (Value < ((1ull << 21) - 1)) {
+    n_bytes = 3;
+    result |= (1ull << 15) | (1ull << 7);
+  } else if (Value < ((1ull << 28) - 1)) {
+    n_bytes = 4;
+    result |= (1ull << 23) | (1ull << 15) | (1ull << 7);
+  } else if (Value < ((1ull << 35) - 1)) {
+    n_bytes = 5;
+    result |= (1ull << 31) | (1ull << 23) | (1ull << 15) | (1ull << 7);
+  } else if (Value >= ((1ull << 49) - 1)) {
     n_bytes = 8;
     result |= (1ull << 57) | (1ull << 47) | (1ull << 39) | (1ull << 31) | (1ull << 23) | (1ull << 15) | (1ull << 7);
   } else if (Value >= ((1ull << 42) - 1)) {
@@ -36,19 +58,7 @@ unsigned parallel_encodeULEB128(int64_t Value, uint8_t *p) {
   } else if (Value >= ((1ull << 35) - 1)) {
     n_bytes = 6;
     result |= (1ull << 39) | (1ull << 31) | (1ull << 23) | (1ull << 15) | (1ull << 7);
-  } else if (Value >= ((1ull << 28) - 1)) {
-    n_bytes = 5;
-    result |= (1ull << 31) | (1ull << 23) | (1ull << 15) | (1ull << 7);
-  } else if (Value >= ((1ull << 21) - 1)) {
-    n_bytes = 4;
-    result |= (1ull << 23) | (1ull << 15) | (1ull << 7);
-  } else if (Value >= ((1ull << 14) - 1)) {
-    n_bytes = 3;
-    result |= (1ull << 15) | (1ull << 7);
-  } else if (Value > ((1ull << 7) -1)) {
-    n_bytes = 2;
-    result |= (1ull << 7);
-  }
+  } 
 
   *((uint64_t*)p) = result;
 
